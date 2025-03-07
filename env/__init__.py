87661b27586cf9d3014f81bac8cfa65a1216af2f
sarvan13 <sarvangill13@gmail.com>
2025-03-06 18:06:14 -0800

    Actually adding the env this time

---
+++ env/__init__.py
@@ -0,0 +1,6 @@
+from gymnasium.envs.registration import register
+
+register(
+    id="Quadrotor-v1",
+    entry_point="env.quad.quad_rotor:QuadRateEnv",  # Adjust as per your directory structure
+)
---
+++ env/quad/__init__.py
@@ -0,0 +1,4 @@
+from env.quad.quad_rotor import QuadRateEnv
+# from gym.envs.mujoco.direct_quad import QuadDirectEnv
+# from gym.envs.mujoco.ball_bouncing_quad import BallBouncingQuadEnv
+# from gym.envs.mujoco.overactuated_quad import OveractuatedQuadEnv
