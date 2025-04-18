7e45221809867840f889091efb45bee014ea3cd6
Sarvan Gill <sarvangill13@gmail.com>
2025-04-17 19:53:53 -0700

    Updating PPO hyperparameters

--- run_on_policy.py
+++ run_on_policy.py
@@ -14,7 +14,7 @@ from env.cartpole.cost_pend import CustomInvertedPendulumEnv
 from env.bicycle.bicycle_model import KinematicBicycleEnv
 
 parser = argparse.ArgumentParser(description='Train PPO/LY with command line arguments')
-parser.add_argument('--N', type=int, default=2048, help='Update frequency')
+parser.add_argument('--N', type=int, default=1024, help='Update frequency')
 parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
 parser.add_argument('--n_epochs', type=int, default=10, help='Number of epochs for training')
 parser.add_argument('--entropy_coeff', type=float, default=0.001, help='Entropy coefficient')
@@ -52,11 +52,11 @@ os.makedirs(data_path, exist_ok=True)
 
 if modelType == "ppo":
     agent = PPOAgent(input_dims=env.observation_space.shape[0], n_actions=env.action_space.shape[0], max_action=env.action_space.high, 
-                     batch_size=batch_size, update_freq=update_freq, n_epochs=n_epochs, entropy_coeff=entropy_coeff, save_dir=data_path)
+                     batch_size=batch_size, gamma=0.9, update_freq=update_freq, n_epochs=n_epochs, entropy_coeff=entropy_coeff, save_dir=data_path)
 elif modelType == "ly":
     agent = LYAgent(input_dims=env.observation_space.shape[0], n_actions=env.action_space.shape[0], max_action=env.action_space.high, 
                         dt=env.unwrapped.dt, equilibrium_state=equilibrium_state, entropy_coeff=entropy_coeff,
-                        batch_size=batch_size, update_freq=update_freq, n_epochs=n_epochs, save_dir=data_path)    
+                        batch_size=batch_size, gamma=0.9, update_freq=update_freq, n_epochs=n_epochs, save_dir=data_path)    
 else:
     raise ValueError("Invalid model type")
 
