a9e0d191dd20bdf0a2683f2d53f70e63ec4f416e
Sarvan Singh Gill <sarvan13@narval3.narval.calcul.quebec>
2026-02-25 22:51:22 -0500

    typo

--- queue_lppo_quad.sh
+++ queue_lppo_quad.sh
@@ -2,12 +2,12 @@
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=1:00:0
+#SBATCH --time=10:00:0
 #SBATCH --mail-user=sarvan13@uvic.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-danielac
 #SBATCH --gpus-per-node=1
-#SBATCH --array=1-10
+#SBATCH --array=0-39
 
 cd ~/projects/def-danielac/sarvan13/thesis-lyapunov/off-policy-lyapunov
 module purge
@@ -15,5 +15,11 @@ module load python/3.10.13
 module load mujoco
 source ~/MujocoENV/bin/activate
 
-python clean_lppo.py --env-id Quadrotor-Still-v1 --num-envs 8 --ent-coef 0.001 --save_model --total-timesteps 15000000 --seed $SLURM_ARRAY_TASK_ID
+MU_VALUES=(0.01 0.1 0.5 0.75)
+
+MU_INDEX=$((SLURM_ARRAY_TASK_ID / 10))
+MU=${MU_VALUES[$MU_INDEX]}
+SEED=$(((SLURM_ARRAY_TASK_ID % 10) + 1))
+
+python clean_lppo.py --env-id Quadrotor-Still-v1 --num-envs 8 --ent-coef 0.001 --save_model --total-timesteps 15000000 --seed $SEED --mu $MU
 
